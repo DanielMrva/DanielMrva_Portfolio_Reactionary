f3fa174b482bdeedfbbd4999f2632ce2adeba4fe
Daniel Mrva <mrva.paints@gmail.com>
2022-05-18 22:34:26 -0600

    cards built, starting gh pages deploy process, still needs basically infinite testing

--- src/components/pages/cards/projectCard.js
+++ src/components/pages/cards/projectCard.js
@@ -1,22 +1,26 @@
 import React from "react";
 
-function card(props) {
+function projectCard( {projects}) {
     return (
-
-    );
+        <div>
+            {projects.map(project => (
+            <div className="card">
+                <div className="card-image">
+                    <img src={`./assets/images/${project.image}`}/>
+                    <span className="card-title">{project.name}</span>
+                </div>
+                <div className="card-content">
+                    <p>{project.desc}</p>
+                </div>
+                <div className="card-action">
+                    <a href={project.git}>Repository</a>
+                    <a href={project.dep}>Deployed</a>
+                </div>
+            </div>
+            ))}
+        </div>
+    )
 }
 
-{/* <div class="projects-flex">
-                <!-- each project is contained in card wich is also the anchor for the linked project -->
-                    <div class="card" id="project-1">
-                        <img src="./Assets/images/accretion.png" alt="Accretion website" class="project-img">
-                        <div class="project-title">
-                            <h3>Accretion</h3>
-                                <br>
-                            <p class="project-dscr">HTML/CSS/JS/MySQL/<br>Node/Express/Group</p>
-                            <a href="https://github.com/DanielMrva/accretion">Repo</a>
-                            <span>---</span>
-                            <a href="https://accretion2022.herokuapp.com/">Deployed</a>
-                        </div>
-                    </div>
-</div> */}
+export default projectCard;
+
