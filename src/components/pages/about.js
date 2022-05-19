74c985880f6fe0e67f630cddca1dfcf29399081f
Daniel Mrva <mrva.paints@gmail.com>
2022-05-18 22:49:11 -0600

    continued work on pages

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -2,16 +2,20 @@ import React from 'react';
 
 export default function About() {
   return (
-        <section id="about">
-            
-            <div className="section-heading">
-                <h2>About Me</h2>
-            </div>
-            <div class="section-content">
-                <article>
-                    <p>Greetings! I'm Daniel Mrva.  I am a web developer based in Colorado with a skill-set in HTML, CSS, and JS.  I used to be an artist, perhaps I still am.</p>
-                </article>
-            </div>
-        </section>
+    <section className="section" id="about">
+    <div className="row">
+      <div className="col s12 m3 l3">
+        <h2 className="about-text">About Me</h2>
+      </div>
+
+      <div className="col m9 l9">
+        <p className="about-text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur repudiandae, facilis deleniti iusto amet assumenda. Laborum beatae animi sequi optio iste, sint delectus error assumenda explicabo, officia a harum suscipit!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit accusamus veniam modi quisquam odio ratione possimus, amet, excepturi minima et consequatur earum. Et, at facilis quam suscipit veniam alias veritatis?
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut asperiores sequi tempora, ducimus accusantium minus accusamus optio. Hic, quisquam quaerat perferendis tempora repellendus animi sunt quidem natus sapiente modi doloribus.
+        </p>
+      </div>
+    </div>
+  </section>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "boostrap/dist/css/boostrap.min.css"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
