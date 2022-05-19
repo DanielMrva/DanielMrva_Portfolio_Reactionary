f2a63b50b123cd7fb03479933bff56c90c6cc55d
Daniel Mrva <mrva.paints@gmail.com>
2022-05-19 11:00:00 -0600

    worked on nav-tabs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import PortfolioContainer from './components/PortfolioContainer';
+import './css/materialize.min.css';
 
 const App = () => <PortfolioContainer />;
 
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -2,37 +2,52 @@ import React from "react";
 
 function NavTabs({ currentPage, handlePageChange }) {
     return (
-        <ul className="nav nav-tabs">
-            <li className="nav-item">
-                <a
-                    href="#home"
-                    onClick={() => handlePageChange('Home')}
-                    className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-                >Home</a>
-            </li>
-            <li className="nav-item">
-                <a
-                    href="#about"
-                    onClick={() => handlePageChange('About')}
-                    className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-                >About</a>
-            </li>
-            <li className="nav-item">
-                <a
-                    href="#projects"
-                    onClick={() => handlePageChange('Projects')}
-                    className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
-                >Projects</a>
-            </li>
-            <li className="nav-item">
-                <a
-                    href="#contact"
-                    onClick={() => handlePageChange('Contact')}
-                    className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-                >Contact</a>
-            </li>
-
-        </ul>
+        <div className="divider">
+            <nav className="nav-extended brown lighten-4">
+            <div className="nav-wrapper">
+                <a href="#home"
+                        onClick={() => handlePageChange('Home')}
+                        classNameName="brand-logo"
+                    >Daniel Mrva</a>
+                <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+            <div className="nav-content">
+                <ul className="tabs tabs-transparent">
+                    <li className="tab">
+                        <a
+                            href="#about"
+                            onClick={() => handlePageChange('About')}
+                            classNameName={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+                        >About</a>
+                    </li>
+                    <li className="tab">
+                        <a
+                            href="#projects"
+                            onClick={() => handlePageChange('Projects')}
+                            classNameName={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}
+                        >Projects</a>
+                    </li>
+                    <li className="tab">
+                        <a
+                            href="#contact"
+                            onClick={() => handlePageChange('Contact')}
+                            classNameName={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                        >Contact</a>
+                    </li>
+                </ul>
+            </div>
+            </nav>
+            <ul className="sidenav" id="mobile-demo">
+            <li><a href="#about">About</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+            </ul>
+      </div>
     );
 }
 
