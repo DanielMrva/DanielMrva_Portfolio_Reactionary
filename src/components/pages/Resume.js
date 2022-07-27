bebcc8085e87e1cbb7e755829f21d8355149dd21
Daniel Mrva <mrva.paints@gmail.com>
2022-07-27 17:32:34 -0600

    work on resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,143 +3,162 @@ import React from 'react';
 export default function Resume() {
     return (
         <>
-  <section className="section container">
+            <section className='section container'>
                 <div className='row'>
-                    <div className='col m12 l4'>
-                        <div className='row brown lighten-3 no-bottom-margin'>
-                            <div className='col m12'>
-                                <h4>Daniel <br></br>Mrva</h4>
-                                <ul>
-                                    <li><i className="fa-solid fa-paper-plane"></i>mrva.develops@gmail.com</li>
-                                    <li><i className="fa-solid fa-location-dot"></i>Centennial, CO</li>
-                                    <li><i className="fa-brands fa-github"></i>github.com/DanielMrva</li>
-                                    <li><i className="fa-brands fa-linkedin"></i>linkedin.com/in/daniel-mrva/</li>
-                                </ul>
-                                
-                            </div>
-                        </div>
-                        <div className='row grey lighten-4 no-bottom-margin'>
-                            <div className='col m12 '>
-                                <h5>Education</h5>
-                                <ul>
-                                    <h6>Full Stack Web Dev Bootcamp</h6>
-                                    <ul>
-                                        <li>Technical Certificate</li>
-                                        <li>2022</li>
-                                        <li>University of Denver</li>
-                                    </ul>                           
-                                </ul>
-                                <ul>
-                                    <h6>Master of Fine Arts</h6>
-                                    <ul>
-                                        <li>Painting</li>
-                                        <li>2013</li>
-                                        <li>Indiana University</li>
-                                    </ul>                           
-                                </ul>
-                                <ul>
-                                    <h6>Bachelor of Fine Arts</h6>
-                                    <ul>
-                                        <li>Painting</li>
-                                        <li>2007</li>
-                                        <li>Illinois State University</li>
-                                    </ul>                           
-                                </ul>
-                                <br></br>
-                                <h5>Skills</h5>
-                                <ul>
-                                    <h6>Web Technologies</h6>
-                                    <li>HTML 5</li>
-                                    <li>CSS 3</li>
-                                    <li>JavaScript ES6</li>
-                                    <li>Materialize</li>
-                                    <li>Handlebars</li>
-                                    <li>React / Redux</li>
-                                    <h6>Scripts/Backend</h6> 
-                                    <li>Node JS</li>
-                                    <li>Express JS</li>
-                                    <h6>Database</h6> 
-                                    <li>MySQL / Sequelize</li>
-                                    <li>MongoDB / Mongoose</li>
-                                    <li>GraphQL / Apollo Client</li>
-                                    <h6>Versioning / Deployment</h6> 
-                                    <li>GitHub</li>
-                                    <li>Heroku</li>
-                                </ul>
-                                <br></br>
-                                <br></br>
-                                <br></br>
-                                
-
-                            </div>
-
-                        </div>
-
-                        
-
+                    <div className='col s12'>
+                        <h2 className='center-align'>Daniel Mrva</h2>
                     </div>
-                    <div className='col m12 l8 grey lighten-3'>
+                    <div className='col s12'>
+                        <p className='center-align'>309-2127953 | mrva.develops@gmail.com | linkedin.com/in/daniel-mrva | github.com/DanielMrva</p>
+                    </div>
+                    <div className='col s12'>
+                        <h4 className='left-align'>Education</h4>
+                        <div className='divider'></div>
                         <div className='row'>
-                            <div className='col m12'>
-                                <h5>Career Goals</h5>
-                                <p>A full stack web developer with a background in the arts and education.  A developer driven by systems, optimization, data, and creative problem solving, who loves to tackle a challenge, master it, and then share that knowledge with others.</p>
-                            </div>
-
-                        </div>
-                        <div className='row'>
-                            <div className='col m12'>
-                                <h5>Projects</h5>
-                                <div className='row'>
-                                    <div className="col m12">
-                                        <h6><b>Accretion</b></h6>
-                                        <p>MySQl/Sequelize - Node - Express - Handlebars<br></br>Accretion is weekly report submission and database app built using specifications of a USGS regional office. <br></br>As part of this group project, I focused on the database functionality, and dynamic rendering via Handlebars and Sequelize queries.</p>
-                                    </div>
-                                    <div className="col m12">
-                                        <h6><b>social_net_work</b></h6>
-                                        <p>MongoDB/Mongoose - Express<br></br>A very cleverly named, yet basic social network API built using Express, MongoDB, Mongoose, and utilizing lorum-ipsum module for seed text generation. Particular interest on my part on this app was the logic for seeding the database with randomized data, and subsequent association of randomly generated users with randomly generated comment via queries.</p>
-                                    </div>
-
-                                </div>
-
-
-                            </div>
-                            <div className='col m12 no-bottom-margin'>
-                                <h5>Work Experience</h5>
-                                <div className='row'>
-                                    <div className="col m12">
-                                        <h6>Catering Supervisor/Head Delivery Driver</h6>
-                                        <p><i className='material-icons tiny'>work</i>Jimmy Johns <i className='material-icons tiny'>today</i> May 2017 - Present <i className='material-icons tiny'>room</i>Englewood, CO</p>
-                                        <p>Lead delivery driver in charge of catering, routing, maintaining quality control on catering orders, first point of contact for customer's delivery issues. Assists with morning open, sandwich making, order assembly, etc.</p>
-
-                                    </div>
-                                    <div className="col m12">
-                                        <h6>Purchasing Coordinator / Art Handler</h6>
-                                        <p><i className='material-icons tiny'>work</i>Terry Doud Inc <i className='material-icons tiny'>today</i> January 2014 - Febuary 2017 <i className='material-icons tiny'>room</i>Chicago, IL / Denver, CO</p>
-                                        <p>
-                                        Worked as a materials/equipment purchaser for a small art-handling business (approximately 30 employees between two branches). Accountant's assistant, primarily concerned with reconciling accounts payable to specific jobs/operations. Was the primary liaison between operations, admin, and project management. Tracked inventory between two branches.
-                                        </p>
-                                    </div>
-                                    <div className="col m12">
-                                        <h6>Associate Instructor</h6>
-                                        <p><i className='material-icons tiny'>work</i>Indiana University <i className='material-icons tiny'>today</i> January 2012 - June 2013 <i className='material-icons tiny'>room</i>Bloomington, IN</p>
-                                        <p className='no-bottom-margin'>
-                                            Instructor for a class of approximately 20 undergraduate students in foundations color theory and design course for a total of three semesters.  Worked directly with foundations area professor to implement curriculum and adapt previously designed projects.  Responsible for grading, mantaining class supplies and encouraging positive class atmosphere in a group of diverse students.
-                                        </p>
-                                    </div>
-
-                                </div>
-
-                            </div>
-
+                            
+                            
                         </div>
 
                     </div>
-
                 </div>
-
             </section>
             
         </>
     );
   }
 
+//   <section className="section container">
+//                 <div className='row'>
+//                     <div className='col m12 l4'>
+//                         <div className='row brown lighten-3 no-bottom-margin'>
+//                             <div className='col m12'>
+//                                 <h4>Daniel <br></br>Mrva</h4>
+//                                 <ul>
+//                                     <li><i className="fa-solid fa-paper-plane"></i>mrva.develops@gmail.com</li>
+//                                     <li><i className="fa-solid fa-location-dot"></i>Centennial, CO</li>
+//                                     <li><i className="fa-brands fa-github"></i>github.com/DanielMrva</li>
+//                                     <li><i className="fa-brands fa-linkedin"></i>linkedin.com/in/daniel-mrva/</li>
+//                                 </ul>
+                                
+//                             </div>
+//                         </div>
+//                         <div className='row grey lighten-4 no-bottom-margin'>
+//                             <div className='col m12 '>
+//                                 <h5>Education</h5>
+//                                 <ul>
+//                                     <h6>Full Stack Web Dev Bootcamp</h6>
+//                                     <ul>
+//                                         <li>Technical Certificate</li>
+//                                         <li>2022</li>
+//                                         <li>University of Denver</li>
+//                                     </ul>                           
+//                                 </ul>
+//                                 <ul>
+//                                     <h6>Master of Fine Arts</h6>
+//                                     <ul>
+//                                         <li>Painting</li>
+//                                         <li>2013</li>
+//                                         <li>Indiana University</li>
+//                                     </ul>                           
+//                                 </ul>
+//                                 <ul>
+//                                     <h6>Bachelor of Fine Arts</h6>
+//                                     <ul>
+//                                         <li>Painting</li>
+//                                         <li>2007</li>
+//                                         <li>Illinois State University</li>
+//                                     </ul>                           
+//                                 </ul>
+//                                 <br></br>
+//                                 <h5>Skills</h5>
+//                                 <ul>
+//                                     <h6>Web Technologies</h6>
+//                                     <li>HTML 5</li>
+//                                     <li>CSS 3</li>
+//                                     <li>JavaScript ES6</li>
+//                                     <li>Materialize</li>
+//                                     <li>Handlebars</li>
+//                                     <li>React / Redux</li>
+//                                     <h6>Scripts/Backend</h6> 
+//                                     <li>Node JS</li>
+//                                     <li>Express JS</li>
+//                                     <h6>Database</h6> 
+//                                     <li>MySQL / Sequelize</li>
+//                                     <li>MongoDB / Mongoose</li>
+//                                     <li>GraphQL / Apollo Client</li>
+//                                     <h6>Versioning / Deployment</h6> 
+//                                     <li>GitHub</li>
+//                                     <li>Heroku</li>
+//                                 </ul>
+//                                 <br></br>
+//                                 <br></br>
+//                                 <br></br>
+                                
+
+//                             </div>
+
+//                         </div>
+
+                        
+
+//                     </div>
+//                     <div className='col m12 l8 grey lighten-3'>
+//                         <div className='row'>
+//                             <div className='col m12'>
+//                                 <h5>Career Goals</h5>
+//                                 <p>A full stack web developer with a background in the arts and education.  A developer driven by systems, optimization, data, and creative problem solving, who loves to tackle a challenge, master it, and then share that knowledge with others.</p>
+//                             </div>
+
+//                         </div>
+//                         <div className='row'>
+//                             <div className='col m12'>
+//                                 <h5>Projects</h5>
+//                                 <div className='row'>
+//                                     <div className="col m12">
+//                                         <h6><b>Accretion</b></h6>
+//                                         <p>MySQl/Sequelize - Node - Express - Handlebars<br></br>Accretion is weekly report submission and database app built using specifications of a USGS regional office. <br></br>As part of this group project, I focused on the database functionality, and dynamic rendering via Handlebars and Sequelize queries.</p>
+//                                     </div>
+//                                     <div className="col m12">
+//                                         <h6><b>social_net_work</b></h6>
+//                                         <p>MongoDB/Mongoose - Express<br></br>A very cleverly named, yet basic social network API built using Express, MongoDB, Mongoose, and utilizing lorum-ipsum module for seed text generation. Particular interest on my part on this app was the logic for seeding the database with randomized data, and subsequent association of randomly generated users with randomly generated comment via queries.</p>
+//                                     </div>
+
+//                                 </div>
+
+
+//                             </div>
+//                             <div className='col m12 no-bottom-margin'>
+//                                 <h5>Work Experience</h5>
+//                                 <div className='row'>
+//                                     <div className="col m12">
+//                                         <h6>Catering Supervisor/Head Delivery Driver</h6>
+//                                         <p><i className='material-icons tiny'>work</i>Jimmy Johns <i className='material-icons tiny'>today</i> May 2017 - Present <i className='material-icons tiny'>room</i>Englewood, CO</p>
+//                                         <p>Lead delivery driver in charge of catering, routing, maintaining quality control on catering orders, first point of contact for customer's delivery issues. Assists with morning open, sandwich making, order assembly, etc.</p>
+
+//                                     </div>
+//                                     <div className="col m12">
+//                                         <h6>Purchasing Coordinator / Art Handler</h6>
+//                                         <p><i className='material-icons tiny'>work</i>Terry Doud Inc <i className='material-icons tiny'>today</i> January 2014 - Febuary 2017 <i className='material-icons tiny'>room</i>Chicago, IL / Denver, CO</p>
+//                                         <p>
+//                                         Worked as a materials/equipment purchaser for a small art-handling business (approximately 30 employees between two branches). Accountant's assistant, primarily concerned with reconciling accounts payable to specific jobs/operations. Was the primary liaison between operations, admin, and project management. Tracked inventory between two branches.
+//                                         </p>
+//                                     </div>
+//                                     <div className="col m12">
+//                                         <h6>Associate Instructor</h6>
+//                                         <p><i className='material-icons tiny'>work</i>Indiana University <i className='material-icons tiny'>today</i> January 2012 - June 2013 <i className='material-icons tiny'>room</i>Bloomington, IN</p>
+//                                         <p className='no-bottom-margin'>
+//                                             Instructor for a class of approximately 20 undergraduate students in foundations color theory and design course for a total of three semesters.  Worked directly with foundations area professor to implement curriculum and adapt previously designed projects.  Responsible for grading, mantaining class supplies and encouraging positive class atmosphere in a group of diverse students.
+//                                         </p>
+//                                     </div>
+
+//                                 </div>
+
+//                             </div>
+
+//                         </div>
+
+//                     </div>
+
+//                 </div>
+
+//             </section>
